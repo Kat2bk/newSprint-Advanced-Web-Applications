2a529f6ee685e05a0b707bde078388ad11f3463c
unknown <Katherine2bk@gmail.com>
2020-09-12 10:03:06 -0700

    created a function called fetchColors, set in useEffect in BubblePage.js

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import {axiosWithAuth} from "../utils/axiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
@@ -8,10 +8,26 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+  const [colors, setColors] = useState([]);
+
+  useEffect(() => {
+    fetchColors();
+  }, [colors])
+
+  const fetchColors = () => {
+    axiosWithAuth()
+    .get("/colors")
+    .then(response => {
+      setColorList(response.data)
+    })
+    .catch(error => {
+      console.log("Unable to get colors", error)
+    })
+  }
 
   return (
     <>
-      <ColorList colors={colorList} updateColors={setColorList} />
+      <ColorList colors={colorList} updateColors={setColorList} colors={colors}/>
       <Bubbles colors={colorList} />
     </>
   );
